da91bb3caa33caab74c0e3a197326e1c909ce9e2
ProdanRaduMatei <51402604+ProdanRaduMatei@users.noreply.github.com>
2021-09-20 08:41:59 +0300

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -10,6 +10,8 @@ import {
 } from "react-native";
 import { useTheme } from '@react-navigation/native';
 
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 const { width, height } = Dimensions.get("window");
 
 const HomeScreen = () => {
@@ -33,6 +35,20 @@ const HomeScreen = () => {
             <View style={styles.textContent}>
               <Text style={styles.cardDescription}>Empowering tech founders to build innovative digital solutions and the next global companies.</Text>
             </View>
+            <View style={styles.statusBar}>
+              <View style={styles.statusBarElements}>
+                <Ionicons name="stopwatch-outline" size={32} color={"#fff"} style={styles.iconStatus}/>
+                <Text style={styles.statusScore}>10</Text>
+              </View>
+              <View style={styles.statusBarElements}>
+                <Ionicons name="speedometer-outline" size={32} color={"#fff"} style={styles.iconStatus}/>
+                <Text style={styles.statusScore}>4.2</Text>
+              </View>
+              <View style={styles.statusBarElements}>
+                <Ionicons name="map-outline" size={32} color={"#fff"} style={styles.iconStatus}/>
+                <Text style={styles.statusScore}>89</Text>
+              </View>
+            </View>
           </View>
           <View style={styles.card}>
             <Image
@@ -42,6 +58,116 @@ const HomeScreen = () => {
             />
             <View style={styles.textContent}>
                 <Text style={styles.cardDescription}>For your safety. Innovative solutions for urban traffic.</Text>
+            </View>
+            <View style={styles.statusBar}>
+              <View style={styles.statusBarElements}>
+                <Ionicons name="stopwatch-outline" size={32} color={"#fff"} style={styles.iconStatus}/>
+                <Text style={styles.statusScore}>10</Text>
+              </View>
+              <View style={styles.statusBarElements}>
+                <Ionicons name="speedometer-outline" size={32} color={"#fff"} style={styles.iconStatus}/>
+                <Text style={styles.statusScore}>4.2</Text>
+              </View>
+              <View style={styles.statusBarElements}>
+                <Ionicons name="map-outline" size={32} color={"#fff"} style={styles.iconStatus}/>
+                <Text style={styles.statusScore}>89</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.card}>
+            <Image
+              source={require("../assets/jobs/netmatch.png")}
+              style={styles.cardImage}
+              resizeMode="cover"
+            />
+            <View style={styles.textContent}>
+              <Text style={styles.cardDescription}>Dutch software specialist focusing on e-commerce and digital transformation.</Text>
+            </View>
+            <View style={styles.statusBar}>
+              <View style={styles.statusBarElements}>
+                <Ionicons name="stopwatch-outline" size={32} color={"#fff"} style={styles.iconStatus}/>
+                <Text style={styles.statusScore}>10</Text>
+              </View>
+              <View style={styles.statusBarElements}>
+                <Ionicons name="speedometer-outline" size={32} color={"#fff"} style={styles.iconStatus}/>
+                <Text style={styles.statusScore}>4.2</Text>
+              </View>
+              <View style={styles.statusBarElements}>
+                <Ionicons name="map-outline" size={32} color={"#fff"} style={styles.iconStatus}/>
+                <Text style={styles.statusScore}>89</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.card}>
+            <Image
+              source={require("../assets/jobs/wolfpack.jpg")}
+              style={styles.cardImage}
+              resizeMode="cover"
+            />
+            <View>
+              <Text style={styles.cardDescription}>We help global brands design, build and launch successful digital products!</Text>
+            </View>
+            <View style={styles.statusBar}>
+              <View style={styles.statusBarElements}>
+                <Ionicons name="stopwatch-outline" size={32} color={"#fff"} style={styles.iconStatus}/>
+                <Text style={styles.statusScore}>10</Text>
+              </View>
+              <View style={styles.statusBarElements}>
+                <Ionicons name="speedometer-outline" size={32} color={"#fff"} style={styles.iconStatus}/>
+                <Text style={styles.statusScore}>4.2</Text>
+              </View>
+              <View style={styles.statusBarElements}>
+                <Ionicons name="map-outline" size={32} color={"#fff"} style={styles.iconStatus}/>
+                <Text style={styles.statusScore}>89</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.card}>
+            <Image
+              source={require("../assets/jobs/inventiff.jpg")}
+              style={styles.cardImage}
+              resizeMode="cover"
+            />
+            <View>
+              <Text style={styles.cardDescription}>Various projects in areas such as software engineering, digital marketing, SEO and graphic design.</Text>
+            </View>
+            <View style={styles.statusBar}>
+              <View style={styles.statusBarElements}>
+                <Ionicons name="stopwatch-outline" size={32} color={"#fff"} style={styles.iconStatus}/>
+                <Text style={styles.statusScore}>10</Text>
+              </View>
+              <View style={styles.statusBarElements}>
+                <Ionicons name="speedometer-outline" size={32} color={"#fff"} style={styles.iconStatus}/>
+                <Text style={styles.statusScore}>4.2</Text>
+              </View>
+              <View style={styles.statusBarElements}>
+                <Ionicons name="map-outline" size={32} color={"#fff"} style={styles.iconStatus}/>
+                <Text style={styles.statusScore}>89</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.card}>
+            <Image
+              source={require("../assets/jobs/msg-romania.jpg")}
+              style={styles.cardImage}
+              resizeMode="cover"
+            />
+            <View>
+              <Text style={styles.cardDescription}>We build innovative solutions for Enterprise customers.</Text>
+            </View>
+            <View style={styles.statusBar}>
+              <View style={styles.statusBarElements}>
+                <Ionicons name="stopwatch-outline" size={32} color={"#fff"} style={styles.iconStatus}/>
+                <Text style={styles.statusScore}>10</Text>
+              </View>
+              <View style={styles.statusBarElements}>
+                <Ionicons name="speedometer-outline" size={32} color={"#fff"} style={styles.iconStatus}/>
+                <Text style={styles.statusScore}>4.2</Text>
+              </View>
+              <View style={styles.statusBarElements}>
+                <Ionicons name="map-outline" size={32} color={"#fff"} style={styles.iconStatus}/>
+                <Text style={styles.statusScore}>10</Text>
+              </View>
             </View>
           </View>
         </Animated.ScrollView>
@@ -62,7 +188,29 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
   },
+  statusBarElements: {
+    flexDirection: 'column',
+    paddingHorizontal: 45,
+    alignItems: 'center',
+  },
+  statusScore: {
+    fontSize: 20,
+    fontWeight: "800",
+    color: "#fff",
+  },
+  iconStatus: {
+    paddingTop: 10,
+    paddingBottom: 2,
+  },
+  statusBar: {
+    backgroundColor: "#00b300",
+    height: 82,
+    flexDirection: 'row',
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+  },
   card: {
+    marginBottom: 10,
     elevation: 2,
     backgroundColor: "#8c8c8c",
     borderRadius: 10,
@@ -71,7 +219,7 @@ const styles = StyleSheet.create({
     shadowRadius: 5,
     shadowOpacity: 0.3,
     shadowOffset: { x: 2, y: -2 },
-    height: 220,
+    minHeight: 250,
     width: "90%",
     alignSelf: "center",
   },
@@ -82,6 +230,8 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 10,
     width: "100%",
     height: "100%",
+    maxHeight: 110,
+    minHeight: 90,
     alignSelf: "center",
   },
   textContent: {
@@ -95,7 +245,12 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   cardDescription: {
-    fontSize: 18,
+    fontSize: 22,
+    minHeight: 100,
+    fontWeight: "400",
+    paddingHorizontal: 12,
+    paddingTop: 10,
+    paddingBottom: 10,
     color: "#f2f2f2",
 
   },
